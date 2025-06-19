6fd387ced714d4059ba7695c1225f832d715f63c
shmakota <47482311+shmakota@users.noreply.github.com>
2025-06-19 18:54:52 -0500

    remove fishing magnet stuff for now

--- src/iuse_actor.h
+++ src/iuse_actor.h
@@ -452,30 +452,6 @@ class change_scent_iuse : public iuse_actor
 /**
  * This iuse contains the logic to summon an npc on the map.
  */
-class foraging_item_iuse : public iuse_actor
-{
-    public:
-        std::string item_category_name;
-        std::string failure_message;
-        std::string completion_message;
-        std::string invalid_message;
-        std::string need_charges_msg;
-        std::vector<std::string> required_tile;
-        int one_in_amount = 10;
-        int need_charges = 1;
-        int moves = 100;
-
-
-        foraging_item_iuse() : iuse_actor( "foraging_item" ) { }
-        ~foraging_item_iuse() override = default;
-        void load( const JsonObject &obj ) override;
-        int use( player &, item &, bool, const tripoint & ) const override;
-        std::unique_ptr<iuse_actor> clone() const override;
-};
-
-/**
- * This iuse contains the logic to summon an npc on the map.
- */
 class place_npc_iuse : public iuse_actor
 {
     public:
