d057052a5886b366d7788a81893da2537c067774
WishDuck <github@johnrosshiggins.com>
2026-07-28 14:08:18 -0700

    fix: monster aggro no longer early returns (#9977)
    * fix: monster aggro no longer early returns
    
    * missed that it was player only block, add npc block immediately following

--- src/monster.cpp
+++ src/monster.cpp
@@ -1805,18 +1805,6 @@ auto monster::attitude( const Character *u ) const -> monster_attitude
     }
 
     const auto *np = u == nullptr ? nullptr : u->as_npc();
-    if( np != nullptr ) {
-        const auto faction_att = faction.obj().attitude( np->get_monster_faction() );
-        if( faction_att == MFA_FRIENDLY ) {
-            return MATT_FRIEND;
-        }
-        if( faction_att == MFA_NEUTRAL ) {
-            return MATT_IGNORE;
-        }
-        if( faction_att == MFA_HATE ) {
-            return MATT_ATTACK;
-        }
-    }
 
     int effective_anger  = anger;
     int effective_morale = morale;
@@ -1933,6 +1921,17 @@ auto monster::attitude( const Character *u ) const -> monster_attitude
     if( u != nullptr && u->is_player() ) {
         static const auto player_faction = mfaction_id( "player" );
         const auto faction_att = faction.obj().attitude( player_faction );
+        if( faction_att == MFA_HATE ) {
+            return MATT_ATTACK;
+        }
+        if( effective_anger < 10 && faction_att == MFA_FRIENDLY ) {
+            return MATT_FRIEND;
+        }
+        if( effective_anger < 10 && faction_att == MFA_NEUTRAL ) {
+            return MATT_IGNORE;
+        }
+    } else if( u != nullptr && u->is_npc() ) {
+        const auto faction_att = faction.obj().attitude( np->get_monster_faction() );
         if( faction_att == MFA_HATE ) {
             return MATT_ATTACK;
         }
