da6d42af0feddfafdc1c97c27850bd1069253f04
WishDuck <github@johnrosshiggins.com>
2026-08-10 08:32:00 -0700

    fix: dodge tests no longer fail (#10051)
    * fix: fix all dodge tests failing
    
    * format

--- src/character.cpp
+++ src/character.cpp
@@ -6655,9 +6655,9 @@ float Character::get_dodge_base() const
 {
     /** @EFFECT_DEX increases dodge base */
     /** @EFFECT_DODGE increases dodge_base */
-    return get_dex() / 4.0f + has_active_bionic( bionic_id( bio_cqb ) ) ? std::max( get_skill_level(
-                skill_dodge ), BIO_CQB_LEVEL ) : get_skill_level(
-               skill_dodge );
+    return get_dex() / 4.0f + ( has_active_bionic( bionic_id( bio_cqb ) ) ? std::max( get_skill_level(
+                                    skill_dodge ), BIO_CQB_LEVEL ) : get_skill_level(
+                                    skill_dodge ) );
 }
 float Character::get_hit_base() const
 {
