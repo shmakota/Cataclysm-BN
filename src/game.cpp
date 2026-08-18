87e32a0cd05f015f0af69173b4ddfa82bb9d9228
WishDuck <github@johnrosshiggins.com>
2026-08-18 13:09:52 -0700

    fix: refresh visibility cache when it is suppost to be refreshed (#10104)

--- src/game.cpp
+++ src/game.cpp
@@ -2123,6 +2123,7 @@ bool game::do_turn()
     {
         ZoneScopedN( "do_turn_pre_action_updates" );
         perhaps_add_random_npc();
+        refresh_player_visibility_cache_if_needed();
         process_voluntary_act_interrupt();
         process_activity();
         update_performance_bubble();
